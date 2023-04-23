70457c0373d32bf4fc09fb2ee0931b2a6541b3ee
Riesling1623 <ninh.nguyn1602@gmail.com>
2023-04-23 11:04:47 +0700

    Add Java files in Lab01

---
+++ Lab01/src/ChoosingOption.java
@@ -0,0 +1,14 @@
+// Exercise 6.1 
+import javax.swing.JOptionPane;
+public class ChoosingOption {
+    public static void main(String[] args){
+        int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?", null, JOptionPane.YES_OPTION);
+        JOptionPane.showMessageDialog(null, "You've chosen: " + (option == JOptionPane.YES_OPTION?"Yes":"No"));
+        System.exit(0);
+    }
+}
+
+// Questions:
+//     - If users choose "Cancel", It prints "You've chosen: No"
+//     - Change the integer option:
+//          int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?", null, JOptionPane.YES_OPTION);
