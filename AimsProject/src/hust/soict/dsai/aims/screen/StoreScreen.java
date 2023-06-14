55a6bf21f93e2ce7861143ecbf575c4ffd649e00
Riesling1623 <ninh.nguyn1602@gmail.com>
2023-06-14 11:45:34 +0700

    Add StoreScreen

---
+++ AimsProject/src/hust/soict/dsai/aims/screen/StoreScreen.java
@@ -0,0 +1,9 @@
+package hust.soict.dsai.aims.screen;
+
+import javax.swing.JFrame;
+
+import hust.soict.dsai.aims.store.Store;
+
+public class StoreScreen extends JFrame {
+    private Store store;
+}
