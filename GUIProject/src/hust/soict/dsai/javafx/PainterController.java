bb975f32c4c4d5396f5bb80baf58be9db80fd0ca
Riesling1623 <ninh.nguyn1602@gmail.com>
2023-06-22 13:29:25 +0700

    Cart Screen

--- GUIProject/src/hust/soict/dsai/javafx/PainterController.java
+++ GUIProject/src/hust/soict/dsai/javafx/PainterController.java
@@ -6,11 +6,16 @@ import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 
 public class PainterController {
     @FXML
     private Pane drawingAreaPane;
+
+    @FXML
+    private ToggleGroup identical;
 
     @FXML
     void clearButtonPressed(ActionEvent event) {
@@ -19,7 +24,15 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
-        drawingAreaPane.getChildren().add(newCircle);
+        RadioButton selecteRadioButton = (RadioButton) identical.getSelectedToggle();
+        if (selecteRadioButton != null){
+            if (selecteRadioButton.getText().equals("Eraser")){
+                Circle eraserCircle = new Circle(event.getX(), event.getY(), 10, Color.WHITE);
+                drawingAreaPane.getChildren().add(eraserCircle);
+            } else {
+                Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+                drawingAreaPane.getChildren().add(newCircle);
+            }
+        }
     }
 }
